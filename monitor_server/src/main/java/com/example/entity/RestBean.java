e03f986877e68e87d9d15c3b361f58dd7f766297
Zonglin-He <hezonglin657@gmail.com>
2025-10-10 14:04:18 +0800

    12

--- monitor_server/src/main/java/com/example/entity/RestBean.java
+++ monitor_server/src/main/java/com/example/entity/RestBean.java
@@ -2,29 +2,49 @@ package com.example.entity;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.JSONWriter;
+import org.slf4j.MDC;
 
-public record RestBean<T> (int code, T data, String message) {
-    public static <T> RestBean<T> success (T data) {
-        return new RestBean<T> (200, data, "Success");
-    }
+import java.util.Optional;
 
-    public static <T> RestBean<T> success () {
-        return success (null);
-    }
+public record RestBean<T> (long id, int code, T data, String message) {
+        public static <T> com.example.entity.RestBean<T> success(T data){
+            return new com.example.entity.RestBean<>(requestId(), 200, data, "Successful");
+        }
 
-    public static <T> RestBean<T> forbidden (String message) {
-        return failure(403,message);
-    }
+        public static <T> com.example.entity.RestBean<T> success(){
+            return success(null);
+        }
 
-    public static <T> RestBean<T> unauthorized (String message) {
-        return failure(401,message);
-    }
+        public static <T> com.example.entity.RestBean<T> forbidden(String message){
+            return failure(403, message);
+        }
 
-    public static <T> RestBean<T> failure (int code, String message) {
-        return new RestBean<T> (code, null, message);
-    }
+        public static <T> com.example.entity.RestBean<T> unauthorized(String message){
+            return failure(401, message);
+        }
 
-    public String asJsonString() {
-        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
-    }
+        public static <T> com.example.entity.RestBean<T> failure(int code, String message){
+            return new com.example.entity.RestBean<>(requestId(), code, null, message);
+        }
+
+        public static <T> com.example.entity.RestBean<T> noPermission() {
+            return new com.example.entity.RestBean<>(requestId(), 401, null, "Insufficient permissions, access denied");
+        }
+
+        /**
+         * 快速将当前实体转换为JSON字符串格式
+         * @return JSON字符串
+         */
+        public String asJsonString() {
+            return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+        }
+
+        /**
+         * 获取当前请求ID方便快速定位错误
+         * @return ID
+         */
+        private static long requestId(){
+            String requestId = Optional.ofNullable(MDC.get("reqId")).orElse("0");
+            return Long.parseLong(requestId);
+        }
 }
